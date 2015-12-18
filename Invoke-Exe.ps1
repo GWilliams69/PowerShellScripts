074beb708399acee644496339d100f7ce82f7754
GW69 <hgma@comcast.net>
2015-12-18 16:19:17 -0600

    Signed-off-by: GW69 <hgma@comcast.net>

---
+++ Invoke-Exe.ps1
@@ -0,0 +1,31 @@
+Function Invoke-Exe{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+
+    param(
+        [parameter(mandatory=$true,position=0)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Executable,
+
+        [parameter(mandatory=$true,position=1)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Arguments,
+
+        [parameter(mandatory=$false,position=2)]
+        [ValidateNotNullOrEmpty()]
+        [int]
+        $SuccessfulReturnCode = 0
+    )
+
+    Write-Verbose "Running $ReturnFromEXE = Start-Process -FilePath $Executable -ArgumentList $Arguments -NoNewWindow -Wait -Passthru"
+    $ReturnFromEXE = Start-Process -FilePath $Executable -ArgumentList $Arguments -NoNewWindow -Wait -Passthru
+
+    Write-Verbose "Returncode is $($ReturnFromEXE.ExitCode)"
+
+    if(!($ReturnFromEXE.ExitCode -eq $SuccessfulReturnCode)) {
+        throw "$Executable failed with exit code $($ReturnFromEXE.ExitCode)"
+    }
+}
+
+Invoke-Exe c:\users\gwilliams\desktop\silent.exe /S
